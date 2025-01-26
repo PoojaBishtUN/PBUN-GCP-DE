0295834e039ae912b16e1a2ecd71dde8dc23fea8
PoojaBisht2 <103634655+PoojaBishtUN@users.noreply.github.com>
2025-01-26 22:36:30 +0530

    done final

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,71 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.19.0"
+    }
+  }
+}
+
 provider "google" {
-  region = "asia-south2"
-  project = "de-gcp-201"  
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
-
-# Create a new project
-#resource "google_project" "new_project" {
- # name            = "DE Project"
-  #project_id      = "de-gcp-201"  # Ensure this is globally unique
-  #billing_account = "01C199-5D99B9-8C1FB1"   # Replace with your billing account ID
-  #deletion_policy = "ABANDON"  # Ensure this is set correctly, not "PREVENT"
-#}
-
-# Enable necessary APIs for the project
-resource "google_project_service" "enabled_apis" {
-  for_each = toset([
-    "bigquery.googleapis.com",          # BigQuery API
-    "composer.googleapis.com",          # Cloud Composer API
-    "storage.googleapis.com",           # Cloud Storage API
-    "iam.googleapis.com",               # IAM API
-    "cloudresourcemanager.googleapis.com" # Resource Manager API
-  ])
-  project = google_project.new_project.project_id
- 
-  service = each.key
+resource "google_storage_bucket" "streaming_project_bucket" {
+  name     = var.bucket_name
+  location = var.region
 }
 
-
-
-# Create a storage bucket in the new project
-resource "google_storage_bucket" "new_bkt_tstst" {
-  name     = "de-gcp-201"
-  location = "US"
-#  project  = google_project.new_project.project_id  # Reference the new project ID
-  project = "de-gcp-201" 
-}
-# Create a service account in the new project
-resource "google_service_account" "new_sa_de" {
-  account_id   = "de-test-sa"
-  display_name = "My DE TEST Service Account"
-  project      = "de-gcp-201"  # Reference the correct project ID
+resource "google_pubsub_topic" "topic" {
+  name = var.topic_name
 }
 
-# Assign IAM role to the service account
-resource "google_project_iam_member" "new_sa_role" {
-  role    = "roles/owner"
-  member  = "serviceAccount:${google_service_account.new_sa_de.email}"  # Correctly reference the email of the service account
-  project = "de-gcp-201"
+resource "google_pubsub_subscription" "subscription" {
+  name  = var.subscription_name
+  topic = google_pubsub_topic.topic.name
 }
 
-# Reference the existing service account (manually created)
-#data "google_service_account" "existing_sa" {
-#  account_id = "de-test-sa"  # Use the actual account ID of the manually created service account
-#  project    = "de-gcp-201"   # Specify the project where the service account exists
-#}
-
-# Assign BigQuery Admin role to the service account for the BigQuery dataset
-resource "google_project_iam_member" "bigquery_admin" {
-  project = "de-gcp-201"
-  role    = "roles/bigquery.admin"
-  member  = "serviceAccount:${data.google_service_account.existing_sa.email}"  # Reference the existing service account's email
-}
-
-# Create a BigQuery dataset in the new project
-resource "google_bigquery_dataset" "new-de-datast" {
-  dataset_id = "de_test_ds"
-  location   = "US"
-  #project    = google_project.new_project.project_id  # Reference the new project ID
-  project = "de-gcp-201" 
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = var.dataset_id
+  friendly_name               = "dt_chat"
+  location                    = "US"
+  default_table_expiration_ms = null
 }
